f8cdedd660f27b405f4f91e65f203b6d9775a17b
Shahryar Salmaan <120364854+shahryarsalmaan@users.noreply.github.com>
2023-04-07 22:01:37 +0600

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,84 @@
+const characters = [
+  {
+    name: "Raiden",
+    image: "https://i.imgur.com/HnrLj6n.jpg"
+  },
+  {
+    name: "Collie",
+    image: "https://i.imgur.com/Qt9YC0e.jpg"
+  },
+  {
+    name: "Ganyu",
+    image: "https://i.imgur.com/axviC7m.jpg"
+  },
+  {
+    name: "Xingqiu",
+    image: "https://i.imgur.com/2VVramG.png"
+  },
+  {
+    name: "Ningguang",
+    image: "https://i.imgur.com/qLB0bZm.jpg"
+  },
+  {
+    name: "Albedo",
+    image: "https://i.imgur.com/zE6viXR.jpg"
+  },
+  {
+    name: "Mona",
+    image: "https://i.imgur.com/5mz8zKO.jpg"
+  },
+  {
+    name: "Tianquan",
+    image: "https://i.imgur.com/IcNChJD.jpg"
+  },
+  {
+    name: "Xiangling",
+    image: "https://i.imgur.com/Wvethdl.jpg"
+  },
+  {
+    name: "Shenhe",
+    image: "https://i.imgur.com/x5yvkgV.jpg"
+  },
+  {
+    name: "Bennett",
+    image: "https://i.imgur.com/VjA0WeT.png"
+  },
+  {
+    name: "Diluc",
+    image: "https://i.imgur.com/LXnFQ9e.jpg"
+  },
+  {
+    name: "Shinobu",
+    image: "https://i.imgur.com/ZkpVGDW.jpg"
+  },
+  {
+    name: "Yelan",
+    image: "https://i.imgur.com/0nfm0AS.jpg"
+  },
+  {
+    name: "Kazuha",
+    image: "https://i.imgur.com/JhWqjl9.jpg"
+  },
+  {
+    name: "Diona",
+    image: "https://i.imgur.com/ll7BpX8.png"
+  },
+  {
+    name: "Jean",
+    image: "https://i.imgur.com/o9PPZn8.jpg"
+  },
+  {
+    name: "Xinyan",
+    image: "https://i.imgur.com/CTlIY39.jpg"
+  },
+  {
+    name: "Sucrose",
+    image: "https://i.imgur.com/xJZi7TZ.jpg"
+  },
+  {
+    name: "Faruzan",
+    image: "https://i.imgur.com/2tkL8Y7.png"
+  }
+];
+
+module.exports = characters;
