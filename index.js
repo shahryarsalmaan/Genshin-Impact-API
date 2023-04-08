07d21d1941293831109d0bab9abae2387d6a59a0
Shahryar Salmaan <120364854+shahryarsalmaan@users.noreply.github.com>
2023-04-08 17:30:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const characters = require("./characters");
+const characters_en = require("./characters_en");
+const characters_ar = require("./characters_ar");
 
 const app = express();
 
@@ -7,21 +8,46 @@ app.get("/", (req, res) => {
   res.send("Genshin Impact API for XaviaBot!");
 });
 
-app.get("/character", (req, res) => {
-  const characterNames = characters.map((character) => character.name);
-  const characterImages = characters.map((character) => character.image);
-  const characterData = {
+app.get("/:lang/character", (req, res) => {
+  const lang = req.params.lang;
+  let characterData;
+  switch (lang) {
+    case "en":
+      characterData = characters_en;
+      break;
+    case "ar":
+      characterData = characters_ar;
+      break;
+    default:
+      characterData = characters_en;
+  }
+  const characterNames = characterData.map((character) => character.name);
+  const characterImages = characterData.map((character) => character.image);
+  const characterDataResponse = {
     names: characterNames,
     images: characterImages
   };
-  res.json(characterData);
+  res.json(characterDataResponse);
 });
 
-app.get("/character/:name", (req, res) => {
+app.get("/:lang/character/:name", (req, res, next) => {
+  const lang = req.params.lang;
   const characterName = req.params.name.toLowerCase();
-  const character = characters.find((character) => character.name.toLowerCase() === characterName);
+  let characterData;
+  switch (lang) {
+    case "en":
+      characterData = characters_en;
+      break;
+    case "ar":
+      characterData = characters_ar;
+      break;
+    default:
+      characterData = characters_en;
+  }
+  const character = characterData.find((character) => character.name.toLowerCase() === characterName);
   if (character) {
-    res.json(character);
+    const characterImage = character.image;
+    res.json({ ...character, image: characterImage });
   } else {
     const error = new Error('Character not found');
     error.status = 404;
@@ -29,15 +55,7 @@ app.get("/character/:name", (req, res) => {
   }
 });
 
-app.get("/weapon", (req, res) => {
-  // Code to retrieve weapon data and images
-});
-
-app.get("/weapon/:name", (req, res) => {
-  // Code to retrieve weapon data and image for a specific weapon
-});
-
-// Add more endpoints for other types of images as needed
+// Add endpoints for other types of images as needed
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
