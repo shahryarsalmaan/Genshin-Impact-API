2247fccb078da98de1d4caee762ca7f2f080262b
truong9c2208 <truong9c2208@gmail.com>
2023-04-08 19:55:35 +0700

    small change for Taki-Chan

---
+++ characters.js
@@ -0,0 +1,470 @@
+const characters_en = [
+    {
+        name: "Raiden",
+        image: "https://i.imgur.com/HnrLj6n.jpg"
+    },
+    {
+        name: "Collie",
+        image: "https://i.imgur.com/Qt9YC0e.jpg"
+    },
+    {
+        name: "Ganyu",
+        image: "https://i.imgur.com/axviC7m.jpg"
+    },
+    {
+        name: "Xingqiu",
+        image: "https://i.imgur.com/2VVramG.png"
+    },
+    {
+        name: "Ningguang",
+        image: "https://i.imgur.com/qLB0bZm.jpg"
+    },
+    {
+        name: "Albedo",
+        image: "https://i.imgur.com/zE6viXR.jpg"
+    },
+    {
+        name: "Mona",
+        image: "https://i.imgur.com/5mz8zKO.jpg"
+    },
+    {
+        name: "Tianquan",
+        image: "https://i.imgur.com/IcNChJD.jpg"
+    },
+    {
+        name: "Xiangling",
+        image: "https://i.imgur.com/Wvethdl.jpg"
+    },
+    {
+        name: "Shenhe",
+        image: "https://i.imgur.com/x5yvkgV.jpg"
+    },
+    {
+        name: "Bennett",
+        image: "https://i.imgur.com/VjA0WeT.png"
+    },
+    {
+        name: "Diluc",
+        image: "https://i.imgur.com/LXnFQ9e.jpg"
+    },
+    {
+        name: "Shinobu",
+        image: "https://i.imgur.com/ZkpVGDW.jpg"
+    },
+    {
+        name: "Yelan",
+        image: "https://i.imgur.com/0nfm0AS.jpg"
+    },
+    {
+        name: "Kazuha",
+        image: "https://i.imgur.com/JhWqjl9.jpg"
+    },
+    {
+        name: "Diona",
+        image: "https://i.imgur.com/ll7BpX8.png"
+    },
+    {
+        name: "Jean",
+        image: "https://i.imgur.com/o9PPZn8.jpg"
+    },
+    {
+        name: "Xinyan",
+        image: "https://i.imgur.com/CTlIY39.jpg"
+    },
+    {
+        name: "Sucrose",
+        image: "https://i.imgur.com/xJZi7TZ.jpg"
+    },
+    {
+        name: "Faruzan",
+        image: "https://i.imgur.com/2tkL8Y7.png"
+    },
+    {
+        name: "Klee",
+        image: "https://i.ibb.co/WkNyGP1/xva213.jpg"
+    },
+    {
+        name: "Yanfei",
+        image: "https://i.ibb.co/LSptkKV/xva213.jpg"
+    },
+    {
+        name: "Beidou",
+        image: "https://i.ibb.co/BNML8sG/xva213.jpg"
+    },
+    {
+        name: "Kokomi",
+        image: "https://i.ibb.co/f1trhCT/xva213.jpg"
+    },
+    {
+        name: "Ningguang",
+        image: "https://i.ibb.co/h7BkjTd/xva21.jpg"
+    },
+    {
+        name: "Rosaria",
+        image: "https://i.ibb.co/n6X6CDd/xva213.jpg"
+    },
+    {
+        name: "Sara",
+        image: "https://i.ibb.co/fqvM3Sg/xva213.jpg"
+    },
+    {
+        name: "Yoimiya",
+        image: "https://i.ibb.co/bQQ8Nd0/xva213.jpg"
+    },
+    {
+        name: "Ayato",
+        image: "https://i.ibb.co/cX4DJr7/xva213.jpg"
+    },
+    {
+        name: "Venti",
+        image: "https://i.ibb.co/JRDcW38/xva213.jpg"
+    },
+    {
+        name: "Ayaka",
+        image: "https://i.ibb.co/jrvy5sG/xva213.jpg"
+    },
+    {
+        name: "Gorou",
+        image: "https://i.ibb.co/W68nMDd/xva213.jpg"
+    },
+    {
+        name: "Xiao",
+        image: "https://i.ibb.co/s2shgFM/xva213.jpg"
+    },
+    {
+        name: "Itto",
+        image: "https://i.ibb.co/4dfxMvK/xva213.jpg"
+    },
+    {
+        name: "Heizou",
+        image: "https://i.ibb.co/WPfK731/xva213.jpg"
+    },
+    {
+        name: "Fischl",
+        image: "https://i.ibb.co/k0LYz4N/xva213.jpg"
+    },
+    {
+        name: "Miko",
+        image: "https://i.ibb.co/f9F4Lm7/xva213.jpg"
+    },
+    {
+        name: "Yin Jin",
+        image: "https://i.ibb.co/RC5Gpnx/xva213.jpg"
+    },
+    {
+        name: "Eula",
+        image: "https://i.ibb.co/S51K2ST/xva213.jpg"
+    },
+    {
+        name: "Thoma",
+        image: "https://i.ibb.co/M6sSrjC/xva213.jpg"
+    },
+    {
+        name: "Trataglia",
+        image: "https://i.ibb.co/2yht6F1/xva21.jpg"
+    },
+    {
+        name: "Zhongli",
+        image: "https://i.ibb.co/zQ5znW5/xva213.jpg"
+    },
+    {
+        name: "Aloy",
+        image: "https://i.ibb.co/McFqF5f/xva213.jpg"
+    },
+    {
+        name: "Sayu",
+        image: "https://i.ibb.co/Z86zny9/xva213.jpg"
+    },
+    {
+        name: "Lisa",
+        image: "https://i.ibb.co/8rSRQzF/xva213.jpg"
+    },
+    {
+        name: "Amber",
+        image: "https://i.ibb.co/mN39fbm/xva213.jpg"
+    },
+    {
+        name: "Barbara",
+        image: "https://i.ibb.co/dtyVbGS/xva213.jpg"
+    },
+    {
+        name: "Kaeya",
+        image: "https://i.ibb.co/BfT0xLt/xva213.jpg"
+    },
+    {
+        name: "Xinyan",
+        image: "https://i.ibb.co/PTzdCZQ/xva213.jpg"
+    },
+    {
+        name: "Chongyun",
+        image: "https://i.ibb.co/VNrfFKv/xva213.jpg"
+    },
+    {
+        name: "Razor",
+        image: "https://i.ibb.co/xhx3RFF/xva213.jpg"
+    },
+    {
+        name: "Qiqi",
+        image: "https://i.ibb.co/w49rJg0/xva213.jpg"
+    },
+    {
+        name: "Noelle",
+        image: "https://i.ibb.co/znQ6j0G/xva213.jpg"
+    },
+    {
+        name: "Keqing",
+        image: "https://i.ibb.co/QpXySsp/xva213.jpg"
+    },
+    {
+        name: "Traveler1",
+        image: "https://i.ibb.co/wJ2s9CC/xva21.jpg"
+    },
+    {
+        name: "Traveler2",
+        image: "https://i.ibb.co/sbr5ZmL/xva2.jpg"
+    },
+    {
+        name: "Traveler3",
+        image: "https://i.ibb.co/Mg42Krg/xva21.jpg"
+    }
+];
+
+const characters_ar = [
+    {
+        name: "رايدين",
+        image: "https://i.imgur.com/HnrLj6n.jpg"
+    },
+    {
+        name: "كولي",
+        image: "https://i.imgur.com/Qt9YC0e.jpg"
+    },
+    {
+        name: "غانيو",
+        image: "https://i.imgur.com/axviC7m.jpg"
+    },
+    {
+        name: "شينشو",
+        image: "https://i.imgur.com/2VVramG.png"
+    },
+    {
+        name: "نيلو",
+        image: "https://i.imgur.com/qLB0bZm.jpg"
+    },
+    {
+        name: "الهيثم",
+        image: "https://i.imgur.com/zE6viXR.jpg"
+    },
+    {
+        name: "مونا",
+        image: "https://i.imgur.com/5mz8zKO.jpg"
+    },
+    {
+        name: "طنغري",
+        image: "https://i.imgur.com/IcNChJD.jpg"
+    },
+    {
+        name: "شاولينغ",
+        image: "https://i.imgur.com/Wvethdl.jpg"
+    },
+    {
+        name: "شينهى",
+        image: "https://i.imgur.com/x5yvkgV.jpg"
+    },
+    {
+        name: "بنييت",
+        image: "https://i.imgur.com/VjA0WeT.png"
+    },
+    {
+        name: "ديلوك",
+        image: "https://i.imgur.com/LXnFQ9e.jpg"
+    },
+    {
+        name: "شينبو",
+        image: "https://i.imgur.com/ZkpVGDW.jpg"
+    },
+    {
+        name: "يلان",
+        image: "https://i.imgur.com/0nfm0AS.jpg"
+    },
+    {
+        name: "كازوها",
+        image: "https://i.imgur.com/JhWqjl9.jpg"
+    },
+    {
+        name: "ديونا",
+        image: "https://i.imgur.com/ll7BpX8.png"
+    },
+    {
+        name: "جين",
+        image: "https://i.imgur.com/o9PPZn8.jpg"
+    },
+    {
+        name: "ساينو",
+        image: "https://i.imgur.com/CTlIY39.jpg"
+    },
+    {
+        name: "سكروز",
+        image: "https://i.imgur.com/xJZi7TZ.jpg"
+    },
+    {
+        name: "فاروزان",
+        image: "https://i.imgur.com/2tkL8Y7.png"
+    },
+    {
+        name: "كلي",
+        image: "https://i.ibb.co/WkNyGP1/xva213.jpg"
+    },
+    {
+        name: "يانفى",
+        image: "https://i.ibb.co/LSptkKV/xva213.jpg"
+    },
+    {
+        name: "بيدو",
+        image: "https://i.ibb.co/BNML8sG/xva213.jpg"
+    },
+    {
+        name: "كوكومي",
+        image: "https://i.ibb.co/f1trhCT/xva213.jpg"
+    },
+    {
+        name: "نينغوانغ",
+        image: "https://i.ibb.co/h7BkjTd/xva21.jpg"
+    },
+    {
+        name: "روزاريا",
+        image: "https://i.ibb.co/n6X6CDd/xva213.jpg"
+    },
+    {
+        name: "سارة",
+        image: "https://i.ibb.co/fqvM3Sg/xva213.jpg"
+    },
+    {
+        name: "يوميا",
+        image: "https://i.ibb.co/bQQ8Nd0/xva213.jpg"
+    },
+    {
+        name: "اياتو",
+        image: "https://i.ibb.co/cX4DJr7/xva213.jpg"
+    },
+    {
+        name: "فنتى",
+        image: "https://i.ibb.co/JRDcW38/xva213.jpg"
+    },
+    {
+        name: "اياكا",
+        image: "https://i.ibb.co/jrvy5sG/xva213.jpg"
+    },
+    {
+        name: "جورو",
+        image: "https://i.ibb.co/W68nMDd/xva213.jpg"
+    },
+    {
+        name: "شاو",
+        image: "https://i.ibb.co/s2shgFM/xva213.jpg"
+    },
+    {
+        name: "ايتو",
+        image: "https://i.ibb.co/4dfxMvK/xva213.jpg"
+    },
+    {
+        name: "هايزو",
+        image: "https://i.ibb.co/WPfK731/xva213.jpg"
+    },
+    {
+        name: "فيشيل",
+        image: "https://i.ibb.co/k0LYz4N/xva213.jpg"
+    },
+    {
+        name: "ميكو",
+        image: "https://i.ibb.co/f9F4Lm7/xva213.jpg"
+    },
+    {
+        name: "يونجين",
+        image: "https://i.ibb.co/RC5Gpnx/xva213.jpg"
+    },
+    {
+        name: "ايولا",
+        image: "https://i.ibb.co/S51K2ST/xva213.jpg"
+    },
+    {
+        name: "توما",
+        image: "https://i.ibb.co/M6sSrjC/xva213.jpg"
+    },
+    {
+        name: "تشايلد",
+        image: "https://i.ibb.co/2yht6F1/xva21.jpg"
+    },
+    {
+        name: "زونغلي",
+        image: "https://i.ibb.co/zQ5znW5/xva213.jpg"
+    },
+    {
+        name: "الوي",
+        image: "https://i.ibb.co/McFqF5f/xva213.jpg"
+    },
+    {
+        name: "سايو",
+        image: "https://i.ibb.co/Z86zny9/xva213.jpg"
+    },
+    {
+        name: "ليسا",
+        image: "https://i.ibb.co/8rSRQzF/xva213.jpg"
+    },
+    {
+        name: "امبر",
+        image: "https://i.ibb.co/mN39fbm/xva213.jpg"
+    },
+    {
+        name: "باربارا",
+        image: "https://i.ibb.co/dtyVbGS/xva213.jpg"
+    },
+    {
+        name: "كايا",
+        image: "https://i.ibb.co/BfT0xLt/xva213.jpg"
+    },
+    {
+        name: "شينيان",
+        image: "https://i.ibb.co/PTzdCZQ/xva213.jpg"
+    },
+    {
+        name: "تشونغيون ",
+        image: "https://i.ibb.co/VNrfFKv/xva213.jpg"
+    },
+    {
+        name: "رايزور",
+        image: "https://i.ibb.co/xhx3RFF/xva213.jpg"
+    },
+    {
+        name: "تشي تشي",
+        image: "https://i.ibb.co/w49rJg0/xva213.jpg"
+    },
+    {
+        name: "نويل",
+        image: "https://i.ibb.co/znQ6j0G/xva213.jpg"
+    },
+    {
+        name: "كيتشنغ",
+        image: "https://i.ibb.co/QpXySsp/xva213.jpg"
+    },
+    {
+        name: "ناهيدا",
+        image: "https://i.imgur.com/0gOXCWp.jpg"
+    },
+    {
+        name: "ترافلر1",
+        image: "https://i.ibb.co/wJ2s9CC/xva21.jpg"
+    },
+    {
+        name: "ترافلر2",
+        image: "https://i.ibb.co/sbr5ZmL/xva2.jpg"
+    },
+    {
+        name: "ترافلر3",
+        image: "https://i.ibb.co/Mg42Krg/xva21.jpg"
+    }
+]
+
+module.exports = {
+    characters_ar,
+    characters_en
+}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const characters_en = require("./characters_en");
-const characters_ar = require("./characters_ar");
+const { characters_ar, characters_en } = require("./characters")
 
 const app = express();
 
@@ -10,17 +9,7 @@ app.get("/", (req, res) => {
 
 app.get("/:lang/character", (req, res) => {
   const lang = req.params.lang;
-  let characterData;
-  switch (lang) {
-    case "en":
-      characterData = characters_en;
-      break;
-    case "ar":
-      characterData = characters_ar;
-      break;
-    default:
-      characterData = characters_en;
-  }
+  let characterData = lang == "ar" ? characters_ar : lang == "en" ? characters_en : characters_en;
   const characterNames = characterData.map((character) => character.name);
   const characterImages = characterData.map((character) => character.image);
   const characterDataResponse = {
@@ -33,17 +22,7 @@ app.get("/:lang/character", (req, res) => {
 app.get("/:lang/character/:name", (req, res, next) => {
   const lang = req.params.lang;
   const characterName = req.params.name.toLowerCase();
-  let characterData;
-  switch (lang) {
-    case "en":
-      characterData = characters_en;
-      break;
-    case "ar":
-      characterData = characters_ar;
-      break;
-    default:
-      characterData = characters_en;
-  }
+  let characterData = lang == "ar" ? characters_ar : lang == "en" ? characters_en : characters_en;
   const character = characterData.find((character) => character.name.toLowerCase() === characterName);
   if (character) {
     const characterImage = character.image;
